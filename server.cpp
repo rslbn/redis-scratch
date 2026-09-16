f6774bf5793459008fe0be663bd0aca4f5b373eb
Rony <ronysilaban7@gmail.com>
2026-09-17 01:01:37 +0700

    init: define TCP Server and Client

---
+++ server.cpp
@@ -0,0 +1,69 @@
+#include <cerrno>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+
+static void msg(const char *msg) {
+    fprintf(stderr, "%s/n", msg);
+}
+
+static void die(const char *msg) {
+    int err = errno;
+    fprintf(stdin, "[%d] %s\n", err, msg);
+    std::abort();
+}
+
+static void do_something(int connfd) {
+    char rbuf[64] = {};
+    ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
+    if (n < 0) {
+        msg("read() error");
+        return;
+    }
+    fprintf(stderr, "client says: %s\n", rbuf);
+    char wbuf[] = "world";
+    write(connfd, wbuf, strlen(wbuf));
+}
+
+int main() {
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) {
+        die("socket()");
+    }
+
+    // this is needed for most server applications
+    int val = 1; // related to TIME_WAIT in tcp
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+
+    // bind
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = ntohs(1234); // port
+    addr.sin_addr.s_addr = ntohl(0); // wildcard address 0.0.0.0
+    int rv = bind(fd, (const struct sockaddr *) &addr, sizeof(addr));
+    if (rv) {
+        die("bind()");
+    }
+
+    rv = listen(fd, SOMAXCONN);
+    if (rv) {
+        die("listen()");
+    }
+
+    while(true) {
+        // accept
+        struct sockaddr_in client_addr = {};
+        socklen_t addr_len = sizeof(client_addr);
+        int connfd = accept(fd, (struct sockaddr *) &client_addr, &addr_len);
+        if (connfd < 0) {
+            continue; // error
+        }
+        do_something(connfd);
+        close(connfd);
+    }
+    return 0;
+}
